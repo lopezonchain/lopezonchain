0217fdd9ee730ad9b9e5c0fad7a84e061916ebda
Lopez <ialopez.contacto@gmail.com>
2025-04-09 18:06:45 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,28 @@ const nextConfig = {
   experimental: {
     esmExternals: 'loose'
   },
+  async headers() {
+    return [
+      {
+        // Aplica estos headers a todas las rutas de la aplicación
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // O especifica el origen que necesites
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With,Content-Type,Accept,Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withTM(['@farcaster/frame-sdk'])(nextConfig);
