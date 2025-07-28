97a011741befd56cb8f8d672061b1171be92f007
Lopez <159062276+lopezonchain@users.noreply.github.com>
2025-07-28 15:58:42 +0200

    Update projects

--- src/locales/en.js
+++ src/locales/en.js
@@ -11,13 +11,13 @@ const en = {
   about: {
     title: "About Me",
     description: "I'm Lopez, a full-stack developer with experience in traditional and blockchain software and security.",
-    experience_title:"Experiencie:",
+    experience_title: "Experiencie:",
     experience: "I've worked on challenging projects that merge conventional web technologies with emerging blockchain and cybersecurity solutions. Last year all-in onchain, I've been learning and developing a lot over blockchain networks, including custom smart contracts/programs, web tools and Unity integrations",
-    tech_title:"Tech Stack:",
+    tech_title: "Tech Stack:",
     tech: "I’ve primarily worked with these, though I’m constantly picking up new skills and have tried out a wide range of languages and technologies\nLanguages & frameworks\n Solidity, Rust, Javascript (Node, React, Next, Express), Java (Spring, Android), C#/C++ (Unity, Android)\n Databases\n NoSQL (Mongo, GraphSQL), SQL (PostgreSQL, MySQL, Oracle)\n Systems\n Linux, Android, Windows",
-    passion_title:"Passion:",
+    passion_title: "Passion:",
     passion: "I love building engaging user experiences that are visually striking and functionally robust, where design and technology come together to make a difference.",
-    goal_title:"Goal:",
+    goal_title: "Goal:",
     goal: "To drive digital transformation through scalable, secure, and disruptive solutions."
   },
   projects: {
@@ -26,95 +26,74 @@ const en = {
     list: [
       {
         id: 1,
+        title: "What will be next?",
+        description: "Do you have any project idea and want to bring it to life? lets talk about it!",
+        visitUrl: "https://t.me/lopezdev",
+        image: "/assets/profile.png"
+      },
+      {
+        id: 2,
         title: "WarpPay",
         description: "All-in-one payments miniapp. Easily send, create request links, airdrop or schedule payments. Farcaster and Coinbase feeds integration, also accesible from Desktop.",
         visitUrl: "https://warppay.lopezonchain.xyz",
         image: "/assets/warpPay.png"
       },
       {
-        id: 2,
+        id: 3,
         title: "PingGate",
         description: "Chat based Marketplace. Farcaster and Coinbase feeds integration, also accesible from Desktop.",
         visitUrl: "https://pinggate.lopezonchain.xyz",
         image: "/assets/pingGate.png"
       },
       {
-        id: 3,
-        title: "ETN Buddy",
-        description: "A platform of battles that works with any token, a staking system and much more, in Electroneum",
-        visitUrl: "https://etn.buddybattles.xyz",
-        image: "/assets/buddy.png"
-      },
-      {
         id: 4,
-        title: "Bulk Airdrop Tool",
-        description: "Tool for doing ERC-20 token airdrops to multiple wallets in batches of 500 sends per tx, working on Base and Electroneum",
-        visitUrl: "https://airdrop.buddybattles.xyz",
-        image: "/assets/bulkAirdrop.png"
-      },
-      {
-        id: 5,
         title: "WarpBoard",
         description: "Message board with some contribution and reward mechanics. Farcaster Miniapp, also accesible from Desktop with Metamask. ",
         visitUrl: "https://warpboard.lopezonchain.xyz",
         image: "/assets/warpBoard.png"
-      },
-      {
-        id: 6,
-        title: "Trading bot",
-        description: "Work In Progress - A microservices backend and tg bot for allowing anyone trading seamlessly",
-        visitUrl: "",
-        image: "/assets/wip.png"
-      },
-      {
-        id: 7,
-        title: "What will be next?",
-        description: "Do you have any project idea and want to bring it to life? lets talk about it!",
-        visitUrl: "https://t.me/lopezdev",
-        image: "/assets/profile.png"
       }
     ]
   },
   workHistory: {
     title: "Work History",
     list: [
-      { 
-        id: 1, 
-        role: "Blockchain developer", 
-        company: "No company", 
-        period: "February 2024 - today", 
+      {
+        id: 1,
+        role: "Blockchain developer",
+        company: "No company",
+        period: "February 2024 - today",
         location: "The Earth",
         image: "/assets/workhistory/now.jpg"
       },
-      { 
-        id: 2, 
-        role: "Senior full-stack developer", 
-        company: "Ohtic", 
-        period: "April 2022 - January 2024", 
+      {
+        id: 2,
+        role: "Senior full-stack developer",
+        company: "Ohtic",
+        period: "April 2022 - January 2024",
         location: "Barcelona, Spain",
         image: "/assets/workhistory/ohtic.png"
       },
-      { 
-        id: 3, 
-        role: "Full-stack developer", 
-        company: "Softtek", 
-        period: "September 2019 - March 2022", 
+      {
+        id: 3,
+        role: "Full-stack developer",
+        company: "Softtek",
+        period: "September 2019 - March 2022",
         location: "Ávila, Spain",
         image: "/assets/workhistory/softtek.png"
       },
-      { 
-        id: 4, 
-        role: "Web Developer", 
-        company: "Adcore", 
-        period: "June 2019 - September 2019", 
+      {
+        id: 4,
+        role: "Web Developer",
+        company: "Adcore",
+        period: "June 2019 - September 2019",
         location: "Ávila, Spain",
         image: "/assets/workhistory/adcore.png"
       },
-      { 
-        id: 5, 
-        role: "Intern and Trainee", 
-        company: "everis", 
-        period: "February 2019 - May 2019", 
+      {
+        id: 5,
+        role: "Intern and Trainee",
+        company: "everis",
+        period: "February 2019 - May 2019",
         location: "Salamanca, Spain",
         image: "/assets/workhistory/everis.png"
       }
--- src/locales/es.js
+++ src/locales/es.js
@@ -11,13 +11,13 @@ const es = {
   about: {
     title: "Sobre mí",
     description: "Soy López, un desarrollador full-stack con experiencia en software tradicional, blockchain y seguridad.",
-    experience_title:"Experiencia:",
+    experience_title: "Experiencia:",
     experience: "He trabajado en proyectos desafiantes que fusionan tecnologías web convencionales con soluciones emergentes de blockchain y ciberseguridad. El último año he estado construyendo y aprendiendo en redes blockchain, incluyendo programs y contratos inteligentes personalizados, herramientas web e integraciones Unity",
-    tech_title:"Tecnologías más usadas:",
+    tech_title: "Tecnologías más usadas:",
     tech: "Principalmente he trabajado con esto, aunque siempre estoy aprendiendo y he utilizado muchas más tecnologías. Lenguajes y frameworks\n Solidity, Rust, Javascript (Node, React, Next, Express), Java (Spring, Android), C#/C++ (Unity, Android)\n Bases de datos\n NoSQL (Mongo, GraphSQL), SQL (PostgreSQL, MySQL, Oracle)\n Sistemas\n Linux, Android, Windows ",
-    passion_title:"Pasión:",
+    passion_title: "Pasión:",
     passion: "Me apasiona crear experiencias de usuario envolventes, visualmente impactantes y funcionalmente robustas, donde diseño y tecnología se unen para marcar la diferencia.",
-    goal_title:"Objetivo:",
+    goal_title: "Objetivo:",
     goal: "Impulsar la transformación digital a través de soluciones escalables, seguras y disruptivas."
   },
   projects: {
@@ -26,101 +26,80 @@ const es = {
     list: [
       {
         id: 1,
+        title: "What will be next?",
+        description: "¿Tienes alguna idea que quieras convertir en una realidad? ¡Hablemos!",
+        visitUrl: "https://t.me/lopezdev",
+        image: "/assets/profile.png"
+      },
+      {
+        id: 2,
         title: "WarpPay",
         description: "Miniapp de pagos todo en uno. Envía, crea links de solicitud, multienvíos y pagos programados. Integración en los feeds de Farcaster y Coinbase, soportando también escritorio.",
         visitUrl: "https://warppay.lopezonchain.xyz",
         image: "/assets/warpPay.png"
       },
       {
-        id: 2,
+        id: 3,
         title: "PingGate",
         description: "Chat web3 y marketplace. Integración en los feeds de Farcaster y Coinbase, soportando también escritorio.",
         visitUrl: "https://pinggate.lopezonchain.xyz",
         image: "/assets/pingGate.png"
       },
       {
-        id: 3,
-        title: "ETN Buddy",
-        description: "Una plataforma de batallas que funciona con cualquier token, un sistemas de staking, una herramienta de airdrops y mucho más, en Electroneum",
-        visitUrl: "https://etn.buddybattles.xyz",
-        image: "/assets/buddy.png"
-      },
-      {
         id: 4,
-        title: "Bulk Airdrop Tool",
-        description: "Herramienta para hacer envios masivos de tokens de forma sencilla y rápida en grupos de 500 envíos por transacción, funcionando en Base y Electroneum",
-        visitUrl: "https://airdrop.buddybattles.xyz",
-        image: "/assets/bulkAirdrop.png"
-      },
-      {
-        id: 5,
         title: "WarpBoard",
         description: "Tablón de mensajes con algunas mecánicas de contribuciones y recompensas. Miniapp de Farcaster, también accesible en navegador con Metamask. ",
         visitUrl: "https://warpboard.lopezonchain.xyz",
         image: "/assets/warpBoard.png"
-      },
-      {
-        id: 6,
-        title: "Trading bot",
-        description: "En progreso - Un backend de microservicios y bot de telegram para permitir a cualquier intercambiar tokens de forma sencilla",
-        visitUrl: "",
-        image: "/assets/wip.png"
-      },
-      {
-        id: 7,
-        title: "What will be next?",
-        description: "¿Tienes alguna idea que quieras convertir en una realidad? ¡Hablemos!",
-        visitUrl: "https://t.me/lopezdev",
-        image: "/assets/profile.png"
       }
     ]
   },
   workHistory: {
     title: "Experiencia laboral",
     list: [
-      { 
-        id: 1, 
-        role: "Desarrollador blockchain", 
-        company: "Sin compañía", 
-        period: "Febrero 2024 - hoy", 
+      {
+        id: 1,
+        role: "Desarrollador blockchain",
+        company: "Sin compañía",
+        period: "Febrero 2024 - hoy",
         location: "La Tierra",
         image: "/assets/workhistory/now.jpg"
       },
-      { 
-        id: 2, 
-        role: "Desarrollador full-stack senior", 
-        company: "Ohtic", 
-        period: "Abril 2022 - Enero 2024", 
+      {
+        id: 2,
+        role: "Desarrollador full-stack senior",
+        company: "Ohtic",
+        period: "Abril 2022 - Enero 2024",
         location: "Barcelona, España",
         image: "/assets/workhistory/ohtic.png"
       },
-      { 
-        id: 3, 
-        role: "Desarrollador full-stack", 
-        company: "Softtek", 
-        period: "Septiembre 2019 - Marzo 2022", 
+      {
+        id: 3,
+        role: "Desarrollador full-stack",
+        company: "Softtek",
+        period: "Septiembre 2019 - Marzo 2022",
         location: "Ávila, España",
         image: "/assets/workhistory/softtek.png"
       },
-      { 
-        id: 4, 
-        role: "Desarrollador web", 
-        company: "Adcore", 
-        period: "Junio 2019 - Septiembre 2019", 
+      {
+        id: 4,
+        role: "Desarrollador web",
+        company: "Adcore",
+        period: "Junio 2019 - Septiembre 2019",
         location: "Ávila, España",
         image: "/assets/workhistory/adcore.png"
       },
-      { 
-        id: 5, 
-        role: "Prácticas", 
-        company: "everis", 
-        period: "Febrero 2019 - Mayo 2019", 
+      {
+        id: 5,
+        role: "Prácticas",
+        company: "everis",
+        period: "Febrero 2019 - Mayo 2019",
         location: "Salamanca, España",
         image: "/assets/workhistory/everis.png"
       }
     ]
   },
-  
+
   awards: {
     title: "Premios",
     list: [
